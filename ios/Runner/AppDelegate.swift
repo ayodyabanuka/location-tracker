6922ee92e1619cb2fe695f44cc06621906c1618d
kalika wijemanna <kalikawijemanna@kalikas-MacBook-Air.local>
2022-11-18 10:49:45 +0530

    fixes for ios app

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,6 +8,18 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+      // set the delegate in didFinishLaunchingWithOptions
+      if #available(iOS 10.0, *) {
+          UNUserNotificationCenter.current().delegate = self
+      } else {
+          // Fallback on earlier versions
+      }
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+    // This method will be called when app received push notifications in foreground
+    @available(iOS 10.0, *)
+    override func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void)
+        {
+            completionHandler([.alert, .badge, .sound])
+        }
 }
